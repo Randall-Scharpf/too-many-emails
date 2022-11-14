8dd317cd16ceab1209b049ce3da4bb801a55f407
Randall Scharpf <randall.scharpf@gmail.com>
2022-11-13 22:49:04 -0800

    update endpoints to give better tests of real implementation schema

--- backend/src/endpoints.ts
+++ backend/src/endpoints.ts
@@ -2,22 +2,17 @@ import express from 'express';
 
 const server = express();
 
-server.get("/test-ep", (req, res) => {
-    let weight : number = parseFloat(req.query.pounds.toString());
-    res.send(
-        isNaN(weight) ?
-        "ur momma so fat that her weight isn't even a number" :
-        "ur momma so fat she's " + weight.toFixed(2) + " pounds, which is " + (weight/2.2).toFixed(2) + " kilograms"
-    );
+server.use(express.json());
+server.use(express.urlencoded());
+
+server.get("/echo-get", (req, res) => {
+    res.json(req.query);
+    console.log(req.url);
 })
 
-server.post("/test-post", (req, res) => {
-    let weight : number = parseFloat(req.query.kilos.toString());
-    res.send(
-        isNaN(weight) ?
-        "ur momma so fat that her weight isn't even a number" :
-        "ur momma so fat she's " + (weight*2.2).toFixed(2) + " pounds, which is " + weight.toFixed(2) + " kilograms"
-    );
+server.post("/echo-post", (req, res) => {
+    res.json(req.body);
+    console.log(req.url);
 })
 
 server.listen(80);
