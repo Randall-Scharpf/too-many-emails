42108a57f2ef543185cbb51bfb3a3038bd8ec02f
Vincent Lin <vinlin24@outlook.com>
2022-11-13 19:56:56 -0800

    Clean test code and prepare for push

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -2,35 +2,16 @@
 
 // Or consider using node -r dotenv/config dist/index.js
 import * as dotenv from 'dotenv';
-import * as nodemailer from 'nodemailer';
-import { sendEmail } from './sender';
-import { Email } from './types';
 
-// Load dummy data from .env file
+// Load any environment variables from .env file or shell
 dotenv.config()
-const { EMAIL_ADDRESS, EMAIL_PASSWORD } = process.env;
 
 /**
  * Start the server runtime.
  */
 async function main() {
-  // Define the email object
-  const email: Email = {
-    from: `"Vincent Lin" ${EMAIL_ADDRESS}`,
-    to: new Array("soulace2425@outlook.com"),
-    subject: "Test from Too Many Emails",
-    text: "Hello there once again",
-  };
-
-  const info = await sendEmail(email, EMAIL_ADDRESS, EMAIL_PASSWORD);
-
-  // Message sent: <b658f8ca-6296-ccf4-8306-87d57a0b4321@example.com>
-  console.log(`ID of message sent: ${info.messageId}`);
-
-  // Preview only available when sending through an Ethereal account
-  // Preview URL: https://ethereal.email/message/WaQKMgKddxQDoou...
-  console.log(`Preview URL: ${nodemailer.getTestMessageUrl(info)
-    || '<unavailable>'}`);
+  console.log("Starting the server runtime...");
+  /* TODO */
 }
 
 main().catch(console.error);
