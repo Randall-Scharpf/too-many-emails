04fbb86bfd05b22e53e936638e38b1a42ffcccc4
Vincent Lin <vinlin24@outlook.com>
2022-11-13 17:44:40 -0800

    Enhance and document Email interface

--- backend/src/types.ts
+++ backend/src/types.ts
@@ -2,11 +2,35 @@
 // Define and export interfaces here
 
 export interface Email {
+  /**
+   * Email address of the sender.  It can be in the format 'username@domain.com'
+   * or with a (Unicode supported) name 'Name Here \<username@domain.com\>'.
+  */
   from: string,
-  to: string | Array<string>,
-  cc?: string | Array<string>,
-  bcc?: string | Array<string>,
+  /**
+   * Email addresses to send to.  These support the same format as the sender.
+   */
+  to: Array<string>,
+  /**
+   * Optional email addresses to CC. These support the same format as the
+   * sender.
+   */
+  cc?: Array<string>,
+  /**
+   * Optional email addresses to BCC. These support the same format as the
+   * sender.
+   */
+  bcc?: Array<string>,
+  /**
+   * Optional subject line of the email.
+   */
   subject?: string,
+  /**
+   * Optional body of the email, in plain text.
+   */
   text?: string,
+  /**
+   * Optional body of the email, in HTML.
+   */
   html?: string,
 }
