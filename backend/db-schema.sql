eeb291fe50c8e406a0ab25b23dc012d50e8a368b
Vincent Lin <vinlin24@outlook.com>
2022-11-26 20:17:44 -0800

    Update DB schema file

--- backend/db-schema.sql
+++ backend/db-schema.sql
@@ -7,18 +7,29 @@ CREATE TABLE KeyValue (
 /* Accounts that users register with our application.  We also refer to these
    as "parent emails" or "parent addresses" to disambiguate them from the
    throwaway email addresses that users make within the application.  */
-CREATE TABLE Account (
-    ID INT PRIMARY KEY AUTOINCREMENT,
+CREATE TABLE User (
+    UserID INTEGER PRIMARY KEY AUTOINCREMENT,
     EmailAddress TEXT UNIQUE NOT NULL,
     PasswordHash TEXT NOT NULL, -- SHA-256
     PasswordSalt TEXT NOT NULL -- 8 random ASCII
-    -- TODO: probably needs more columns
 );
 
 /* Addresses that users create within their account.  These are the throwaway
    email addresses intended for use.  */
 CREATE TABLE Address (
-    ID INT PRIMARY KEY AUTOINCREMENT,
-    EmailAddress TEXT UNIQUE NOT NUKL,
-    -- TODO: probably needs more columns
+    AddressID INTEGER PRIMARY KEY AUTOINCREMENT,
+    EmailAddress TEXT UNIQUE NOT NULL,
+    UserID INTEGER,
+    FOREIGN KEY (UserID) REFERENCES User (UserID)
+);
+
+/* An email sent by or received by our client.  */
+CREATE TABLE Email (
+    EmailID INTEGER PRIMARY KEY AUTOINCREMENT,
+    SenderAddress TEXT NOT NULL,
+    ReceiverAddresses TEXT NOT NULL,
+    CCAddresses TEXT,
+    BCCAddresses TEXT,
+    Subject TEXT,
+    Body TEXT
 );
