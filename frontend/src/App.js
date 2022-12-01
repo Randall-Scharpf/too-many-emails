c279ee95fb4b9a9bd1d75d0e4e6ac41b3979f118
Vincent Lin <vinlin24@outlook.com>
2022-12-01 02:12:24 -0800

    Remove routing from App.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,21 +1,10 @@
-import React, { useEffect, useState } from "react";
+import React, { Component } from "react";
 import "./App.css";
-import Header from "./components/Header/Header";
-import Sidebar from "./components/Sidebar/Sidebar";
-import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
-import Mail from "./components/Mail/Mail";
 import EmailList from "./components/EmailList/EmailList";
-import SendMail from "./components/SendMail/SendMail";
-import { useSelector } from "react-redux";
-import { selectSendMessageIsOpen } from "./features/mailSlice";
-import { selectUser } from "./features/userSlice";
-import Login from "./components/Login/Login";
-import Navbar from "./components/Navbar/navbar";
+import Header from "./components/Header/Header";
 import LogInContainer from "./components/LogInContainer/LogInContainer";
-import Login2 from "./components/Login2/Login";
-import Signup from "./components/Signup/Signup";
-import { db } from "./firebase";
-import { Component } from "react";
+import Navbar from "./components/Navbar/navbar";
+import Sidebar from "./components/Sidebar/Sidebar";
 
 const DUMMY_USER = "josie_bruin@ucla.edu"; // REMOVE LATER
 
@@ -28,57 +17,28 @@ class App extends Component {
     };
   }
 
-  // const sendMessageIsOpen = useSelector(selectSendMessageIsOpen);
-  // const user = useSelector(selectUser);
-  // const [emails, setEmails] = useState([]);
-
-  // console.log("emails", emails);
-  // useEffect(() => {
-  //   db.collection("emails")
-  //     .orderBy("timestamp", "desc")
-  //     .onSnapshot((snapshot) =>
-  //       setEmails(
-  //         snapshot.docs.map((doc) => ({
-  //           id: doc.id,
-  //           data: doc.data(),
-  //         }))
-  //       )
-  //     );
-  // }, []);
-
   setAddress(address) {
     this.setState({address});
   }
 
   render() {
     return (
-      <Router>
-        {false ? ( // REVERT TO this.state.user WHEN LOGIN WORKS
-          <div>
-            <Navbar />
-            <LogInContainer />
-
-          </div>
-
-        ) : (
-          <div className="app">
-            <Header />
-            <div className="app-body">
-              <Sidebar user={DUMMY_USER} setAddress={(address) => this.setAddress(address)} />
-              <Switch>
-                <Route path="/mail">
-                  <Mail />
-                </Route>
-                <Route path="/" exact>
+        <div className="root">
+            {false ? ( // REVERT TO this.state.user WHEN LOGIN WORKS
+              <div>
+                <Navbar />
+                <LogInContainer />
+              </div>
+            ) : (
+              <div className="app">
+                <Header />
+                <div className="app-body">
+                  <Sidebar user={DUMMY_USER} setAddress={(address) => this.setAddress(address)} />
                   <EmailList address={this.state.address} />
-                </Route>
-              </Switch>
-            </div>
-
-            {/* {sendMessageIsOpen && <SendMail />} */}
-          </div>
-        )}
-      </Router>
+                </div>
+              </div>
+            )}
+        </div>
     );
   }
 }
