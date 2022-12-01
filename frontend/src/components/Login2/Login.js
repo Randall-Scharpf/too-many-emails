881619d3d34a75b9a4e94bfbc62567cd67e7fc01
Abigail Okonski <abigailokonski1@gmail.com>
2022-12-01 02:46:49 -0800

    Take in user and password login

--- frontend/src/components/Login2/Login.js
+++ frontend/src/components/Login2/Login.js
@@ -1,15 +1,36 @@
-import React from 'react'
+import React, { Component } from 'react'
 import "./Login.css";
 
-const Login = () => {
-  return <div className="Login">
-    <h1>Login</h1>
-    <form>
-      <input type={"email"} placeholder={"Email"} />
-      <input type={"password"} placeholder={"Password"} />
-      <button type={"submit"}>Login</button>
-    </form>
-  </div>;
+
+
+class Login extends Component {
+  constructor() {
+    super();
+    this.state = {
+      user: "",
+      password: "",
+    };
+  }
+
+  checkInput(event) {
+    event.preventDefault();
+
+    alert(this.state.user);
+    alert(this.state.password);
+
+  }
+  render() {
+    return (
+      <div className="Login">
+        <h1>Login</h1>
+        <form onSubmit={(event) => this.checkInput(event)}>
+          <input type={"email"} placeholder={"Email"} onChange={(e) => this.setState({ user: e.target.value })} />
+          <input type={"password"} placeholder={"Password"} onChange={(e) => this.setState({ password: e.target.value })} />
+          <button type={"submit"}>Login</button>
+        </form>
+      </div>
+    )
+  }
 }
 
 export default Login
