5132969c015af77396338b19565d02eef832f152
Vincent Lin <vinlin24@outlook.com>
2022-11-30 18:35:17 -0800

    Enhance clear.js to drop and recreate tables

--- backend/test/clear.js
+++ backend/test/clear.js
@@ -1,9 +1,14 @@
 // clear.js
 // Script for clearing all tables of the database at runtime.
+// It does this by dropping all tables and then executing the schema file.
+// This way, any changes in the schema are also reflected.
 
 const sqlite3 = require("sqlite3");
+const fs = require("node:fs");
 
 const DB_FILE = process.env.DB_FILE || "./.data/sqlite.db";
+const SCHEMA_FILE = process.env.SCHEMA_FILE || "./db-schema.sql";
+
 const db = new (sqlite3.verbose()).Database(DB_FILE);
 
 const TABLE_NAMES = [
@@ -20,7 +25,12 @@ const END = "\x1b[0m";
 console.log(`${RED}Clearing all tables in ${DB_FILE}.${END}`);
 
 TABLE_NAMES.forEach(name => {
-    db.run(`DELETE FROM ${name};`, (err) => {
+    db.run(`DROP TABLE IF EXISTS ${name};`, (err) => {
         err ? console.error(`${RED}${err.message}${END}`) : null;
     });
+});
+
+const schema = fs.readFileSync(SCHEMA_FILE).toString();
+db.exec(schema, (err) => {
+    err ? console.error(`${RED}${err.message}${END}`) : null;
 });
