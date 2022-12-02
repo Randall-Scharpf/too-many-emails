8afb65c8af378cfe51fe1989dcf9ddc4b147b63c
Vincent Lin <vinlin24@outlook.com>
2022-12-01 16:30:19 -0800

    Make login and clicking addrs route back to "/"

--- frontend/src/components/Sidebar/SidebarOption.js
+++ frontend/src/components/Sidebar/SidebarOption.js
@@ -1,16 +1,20 @@
 import React from "react";
+import { useHistory } from "react-router-dom";
 import "./SidebarOption.css";
 
-function SidebarOption({  title, selected, setAddress }) {
-  const openInbox = () =>{
+function SidebarOption({ title, selected, setAddress }) {
+  const history = useHistory();
+  const openInbox = () => {
     setAddress(title);
+    // Make it so that when clicking the addresses while viewing an opened mail,
+    // they're redirected back to the EmailList view
+    history.push("/");
   }
 
-
   return (
-      <button className={`sidebarOption ${selected && "sidebarOption--active"}`} onClick={openInbox}>
-        <h3>{title}</h3>
-      </button>
+    <button className={`sidebarOption ${selected && "sidebarOption--active"}`} onClick={openInbox}>
+      <h3>{title}</h3>
+    </button>
   );
 }
 
